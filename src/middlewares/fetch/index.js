6d7b2c13512956af561f5b9e53fb1ed7b9f584d8
yeliex <yeliex@yeliex.com>
2017-08-16 13:57:29 +0800

    fix some bug with headers

--- src/middlewares/fetch/index.js
+++ src/middlewares/fetch/index.js
@@ -1,9 +1,19 @@
 const fetch = require('../../libs/request');
 
+const headersToObject = (headers) => {
+  if (!(headers instanceof Headers)) {
+    return headers;
+  }
+  return Array.from(headers.keys()).reduce((total, key) => {
+    total[key] = headers.get(key);
+    return total;
+  }, {});
+};
+
 module.exports = () => async (ctx, next) => {
   let index = 1;
 
-  const headers = ctx._HEADERS || {};
+  const headers = headersToObject(ctx._HEADERS || {});
 
   const traceId = ctx.get('X-System-TraceId');
   const rpcId = ctx.get('X-System-RpcId');
