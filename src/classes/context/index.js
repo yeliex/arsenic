eca040b18669871c574c23cdf8e8095d1a124937
yeliex <yeliex@yeliex.com>
2017-08-16 14:30:36 +0800

    reflact Context

--- src/classes/context/index.js
+++ src/classes/context/index.js
@@ -1,10 +1,42 @@
 class Context {
   constructor(ctx) {
-    Object.assign(this, ctx);
+    this.ctx = ctx;
   }
 
-  get ctx() {
-    return this;
+  get logger() {
+    return this.ctx.logger;
+  }
+
+  get controller() {
+    return this.ctx.controller;
+  }
+
+  get service() {
+    return this.ctx.service;
+  }
+
+  get db() {
+    return this.ctx.db;
+  }
+
+  get throw() {
+    return this.ctx.throw;
+  }
+
+  get fetch() {
+    return this.ctx.fetch;
+  }
+
+  get config() {
+    return this.ctx.config;
+  }
+
+  get user() {
+    return this.ctx._USER;
+  }
+
+  get headers() {
+    return this.ctx._HEADERS;
   }
 }
 
