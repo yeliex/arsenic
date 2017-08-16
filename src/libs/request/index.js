2dd3be1ae86de897d9b93f2a3f3dabb80420783f
yeliex <yeliex@yeliex.com>
2017-08-16 17:53:37 +0800

    fetch basehost support node service

--- src/libs/request/index.js
+++ src/libs/request/index.js
@@ -4,7 +4,7 @@ let fetchLog = (require('../../index').Logger || {}).fetch;
 
 fetch.baseHost((path) => {
   const host = process.env.NODE_ENV === 'production' ? '117sport.net' : 'daily.117sport.org';
-  return path.replace(/^\/\/([A-Z]{1,})/, (a, b) => {
+  return path.replace(/^\/\/([A-Z-_]{1,})/, (a, b) => {
     return `http://service-${b.toLowerCase()}.${host}`;
   });
 });
