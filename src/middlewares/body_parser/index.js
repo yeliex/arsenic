f5bcf28a1947da4a8a26112a10209ca4347b875e
yeliex <yeliex@yeliex.com>
2017-09-13 16:30:57 +0800

    add xml body parser

--- src/middlewares/body_parser/index.js
+++ src/middlewares/body_parser/index.js
@@ -2,10 +2,25 @@ const koabody = require('koa-better-body');
 const convert = require('koa-convert');
 const compose = require('koa-compose');
 const qs = require('qs');
+const { toJSON } = require('../../libs/utils/xml');
 
 const extraParser = (app) => async (ctx, next) => {
   ctx._GET = ctx.query;
   ctx._POST = ctx.request.fields;
+  if (!ctx.request.fields && ctx.request.body) {
+    const contentType = ctx.request.type;
+
+    switch (contentType) {
+      case 'text/xml': {
+        ctx._POST = await toJSON(ctx.request.body);
+        break;
+      }
+      default: {
+        ctx._POST = ctx.request.body;
+        break;
+      }
+    }
+  }
   if (ctx._POST) {
     app.logger.apiData.info(`[${ctx.method.toUpperCase()}] ${ctx.originalUrl} ${JSON.stringify(ctx._POST)} ${ctx.request.body}`);
   }
