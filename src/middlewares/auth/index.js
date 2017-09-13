ccc7fa14b323d30c8ff86fdfc8bffafee91230f8
yeliex <yeliex@yeliex.com>
2017-09-13 15:41:37 +0800

    add ignore global auth for outer callback service

--- src/middlewares/auth/index.js
+++ src/middlewares/auth/index.js
@@ -1,4 +1,14 @@
 module.exports = (app) => async (ctx, next) => {
+  if (app.config.noauth === true) {
+    ctx._USER = {
+      userId: 1,
+      companyId: 1,
+      mock: true
+    };
+    await next();
+    return;
+  }
+
   const XUserPassword = ctx.get('x-user-password');
 
   if (XUserPassword !== app.config.X_USER_PASSWORD) {
