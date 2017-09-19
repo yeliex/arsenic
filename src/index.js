ec54ebad44ad451ec832dd9205c2395d9ac977ea
tianshi <tbtianshi@gmail.com>
2017-09-19 13:57:48 +0800

    provide traceId

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ Server.Context = require('./classes/context');
 Server.Router = require('./classes/router');
 Server.Sequelize = require('sequelize');
 Server.Sequelize.public = require('./plugins/db/mysql/public');
+Server.Utils = {
+  traceId: require('./libs/utils/trace')
+};
+
 
 module.exports = Server;
