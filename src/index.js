6435d98f3d25e5f1422a90a147f47799eccb531d
yeliex <yeliex@yeliex.com>
2017-09-21 17:56:46 +0800

    add mongo class

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ exports.BucketUrl = require('./libs/BucketUrl');
 exports.Enums = require('./enums');
 exports.Error = require('./libs/Errors');
 exports.Elastic = require('./libs/Elastic');
+exports.Mongo = require('./libs/Mongo');
 exports.MQ = require('./libs/MQ');
 exports.promisify = require('./libs/promisify').promisify;
 exports.promisifyAll = require('./libs/promisify').promisifyAll;
