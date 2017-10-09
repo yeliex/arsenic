959b0735547607e5a05354ec8d4de10ea8806413
yeliex <yeliex@yeliex.com>
2017-10-09 18:05:31 +0800

    优化错误显示

--- src/libs/error_handler/index.js
+++ src/libs/error_handler/index.js
@@ -2,14 +2,14 @@
 const { Error: BaseError } = require('@bee/foundation');
 
 module.exports = (App) => (error, ctx) => {
-  App.logger.error.error(typeof error.toString === 'function' ? error.toString() : error);
-
   if (error instanceof BaseError) {
+    App.logger.error.error(error.toString());
     ctx.throw(error);
     return;
   }
 
   if (error instanceof Error) {
+    App.logger.error.error(error);
     const newError = new BaseError.Errors.UNKNOWN_ERROR();
     newError.message = error.message;
     ctx.throw(newError);
@@ -17,12 +17,13 @@ module.exports = (App) => (error, ctx) => {
   }
 
   if (!Number.isNaN(error.code) && error.code > 200 && error.code < 512) {
+    App.logger.error.error(JSON.stringify(error));
     ctx.throw(error.code, error.message);
     return;
   }
 
+  App.logger.error.error(error);
   const newError = new BaseError.Errors.UNKNOWN_ERROR();
   newError.message = error.message || error;
   ctx.throw(newError);
 };
-
