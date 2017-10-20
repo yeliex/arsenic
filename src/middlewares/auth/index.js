daeca8d52a03e14496b64ecf03ad334f4225db43
yeliex <yeliex@yeliex.com>
2017-10-20 14:19:09 +0800

    优化在无需权限校验情况下的用户身份

--- src/middlewares/auth/index.js
+++ src/middlewares/auth/index.js
@@ -11,29 +11,26 @@ module.exports = (auth) => async (ctx, next) => {
     auth = !ctx.App.config.noauth;
   }
 
-  // todo: 在这里处理无需校验的情况下获取的用户信息
+  const XUserPassword = ctx.get('x-user-password');
+  const userId = ctx.get('x-user-userid');
 
-  if (!auth) {
+  if (auth && (!XUserPassword || (XUserPassword !== ctx.App.config.X_USER_PASSWORD))) {
+    ctx.throw(401);
+    return;
+  }
+
+  if (userId) {
+    ctx._USER = Object.assign({}, ctx._USER || {}, {
+      userId: Number(ctx.get('X-User-UserId')),
+      companyId: Number(ctx.get('X-User-CompanyId'))
+    });
+  } else {
     ctx._USER = {
       userId: 1,
       companyId: 1,
       mock: true
     };
-    await next();
-    return;
   }
-
-  const XUserPassword = ctx.get('x-user-password');
-
-  if (ctx.App.config.X_USER_PASSWORD && (XUserPassword !== ctx.App.config.X_USER_PASSWORD)) {
-    ctx.throw(401);
-    return;
-  }
-
-  ctx._USER = Object.assign({}, ctx._USER || {}, {
-    userId: Number(ctx.get('X-User-UserId')),
-    companyId: Number(ctx.get('X-User-CompanyId'))
-  });
 
   await next();
 };
