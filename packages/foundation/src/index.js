03c42033c3cabb6a3c158bcc5c7e265b3b4e6eaa
yeliex <yeliex@yeliex.com>
2018-07-12 21:58:52 +0800

    migration

---
+++ packages/foundation/src/index.js
@@ -0,0 +1,7 @@
+exports.Enums = require('./enums/index');
+exports.Error = require('./libs/Errors/index');
+exports.Elastic = require('./libs/Elastic/index');
+exports.Mongo = require('./libs/Mongo/index');
+exports.MQ = require('./libs/MQ/index');
+exports.promisify = require('./libs/promisify/index').promisify;
+exports.promisifyAll = require('./libs/promisify/index').promisifyAll;
--- src/index.js
+++
@@ -1,7 +0,0 @@
-exports.Enums = require('./enums');
-exports.Error = require('./libs/Errors');
-exports.Elastic = require('./libs/Elastic');
-exports.Mongo = require('./libs/Mongo');
-exports.MQ = require('./libs/MQ');
-exports.promisify = require('./libs/promisify').promisify;
-exports.promisifyAll = require('./libs/promisify').promisifyAll;
